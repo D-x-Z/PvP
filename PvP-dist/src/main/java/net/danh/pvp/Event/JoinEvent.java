d123cb4494531afa75a4791c64498afb041a62b2
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-03-25 11:11:46 +0700

    Fix lỗi /pvp on/off

--- PvP-dist/src/main/java/net/danh/pvp/Event/JoinEvent.java
+++ PvP-dist/src/main/java/net/danh/pvp/Event/JoinEvent.java
@@ -1,11 +1,15 @@
 package net.danh.pvp.Event;
 
+import net.danh.Contest.Mining.Mining;
+import net.danh.Manager.*;
 import net.danh.pvp.Manager.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class JoinEvent implements Listener {
 
@@ -14,6 +18,42 @@ public class JoinEvent implements Listener {
         Player p = e.getPlayer();
         if (FirstJoin.isFirstJoin(p)) {
             FirstJoin.setFirstJoin(p, true);
+            if (PlayerData.getXP(p) == 0) {
+                PlayerData.setXP(p, 0);
+            }
+            if (Power.getRestorePower(p) == 0) {
+                Power.setRestorePower(p, Data.getconfigfile().getInt("Restore_Power"));
+            }
+            if (Power.getMaxPower(p) == 0) {
+                Power.setMaxPower(p, Data.getconfigfile().getInt("Max_Power"));
+            }
+            if (Lucky.getLuckyChance(p) == 0) {
+                Lucky.setLuckyChance(p, Data.getconfigfile().getInt("Default_Lucky_Chance"));
+            }
+            if (Power.getPower(p) == 0) {
+                Power.setPower(p, Power.getMaxPower(p));
+            }
+            if (Weight.getWeight(p) == 0) {
+                Weight.setWeight(p, 0);
+            }
+            if (Weight.getMaxWeight(p) == 0) {
+                Weight.setMaxWeight(p, 5000);
+            }
+            if (Bank.getBankLevel(p) == 0) {
+                Bank.setBankLevel(p, 1);
+            }
+            if (Event.getBlockEvent(p) == 0) {
+                Event.setBlocksEvent(p, 0);
+            }
+            if (PlayerData.getPlayerName(p) == null) {
+                PlayerData.setPlayerName(p);
+            }
+            if (Event.getBlockEvent(p) > 0 && !Mining.getStart()) {
+                Event.setBlocksEvent(p, 0);
+            }
+            if (PlayerData.getBlocks(p) <= 0) {
+                PlayerData.setBlocks(p, 0);
+            }
             Points.setPvPPoints(p, Files.getconfigfile().getInt("PVP.DEFAULT_POINTS"));
             ProtectTime.setProtectTimes(p, Files.getconfigfile().getInt("PVP.FIRST_TIME_PROTECT"));
             Status.TogglePvP(p, false);
--- PvP-dist/src/main/java/net/danh/pvp/Manager/Status.java
+++ PvP-dist/src/main/java/net/danh/pvp/Manager/Status.java
@@ -3,22 +3,15 @@ package net.danh.pvp.Manager;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-
 public class Status {
 
     public static boolean getPvPStatus(@NotNull Player p) {
         return Files.getdatafile().getBoolean("players." + p.getName() + ".Status");
     }
 
-    public static void TogglePvP(Player p, boolean status) {
-        if (Points.getPvPPoints(p) >= Files.getconfigfile().getInt("PVP.POINTS_TOGGLE")) {
-            Files.getdatafile().set("players." + p.getName() + ".Status", status);
-            Points.removePvPPoints(p, Files.getconfigfile().getInt("PVP.POINTS_TOGGLE"));
-        } else {
-            p.sendMessage(Files.convert(Objects.requireNonNull(Files.getlanguagefile().getString("PVP.NOT_ENOUGH_POINTS"))
-                    .replaceAll("%amount%", String.valueOf(Files.getconfigfile().getInt("PVP.POINTS_TOGGLE")))));
-        }
+    public static void TogglePvP(@NotNull Player p, boolean status) {
+        Files.getdatafile().set("players." + p.getName() + ".Status", status);
+        Points.removePvPPoints(p, Files.getconfigfile().getInt("PVP.POINTS_TOGGLE"));
         Files.savedata();
     }
 }
